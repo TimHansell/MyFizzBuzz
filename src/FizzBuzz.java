4037513f4fba0e936bc8c531fa41a1db30b6b033
Tim Hansell <hansellt@gmail.com>
2018-12-22 07:59:49 -0500

    Added Test for Multiple of 5

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,9 @@
 public class FizzBuzz {
     public String fizz(Integer inputNumber) {
         if (inputNumber % 3 == 0) {
-            return( "fizz");
+            return ("fizz");
+        } else if (inputNumber % 5 == 0) {
+            return ("buzz");
         } else {
             return (inputNumber.toString());
         }
